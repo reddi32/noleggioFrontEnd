88d43df1f0f5d0f256cccb120101169991666717
reddi32 <rossimatteo32@gmail.com>
2022-10-12 09:27:07 +0200

    Aggiunto il routing per il form di registrazione

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormRegistrazioneComponent } from './formregistrazione/form-registrazione.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'formregistrazione', component: FormRegistrazioneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
